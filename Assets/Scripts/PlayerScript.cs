975b4d2716da016e03caff117f6aa0657e034f9b
Syuen Tong <datasiankidshen@yahoo.com>
2020-10-23 21:29:27 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,23 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
-
     public Text score;
-
+    public Text winText;
+    public Text livesText;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;    
+    private int livesValue = 3;
     private int scoreValue = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        winText.text = "";
+        livesText.text = "Lives: " + livesValue.ToString();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
+        transform.position = new Vector3(0f, 0f, 49.36484f);
     }
 
     // Update is called once per frame
@@ -38,10 +50,37 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3(56.2f, 0.3871047f, 49.36484f);
+                {
+                    if (livesValue <= 2)
+                    {
+                        livesValue = 3;
+                        livesText.text = "Lives: " + livesValue.ToString();
+                    }
+                }
+            }
+            else if (scoreValue ==8)
+            {
+                winText.text = "You Win! Game by Syuen Tong.";
+                musicSource.clip = musicClipTwo;
+                musicSource.Play();
+                musicSource.loop = true;
+            }
         }
-
+        else if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            livesText.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (livesValue <= 0)
+            {
+                winText.text = "You lose! Game by Syuen Tong.";
+            }
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
